7297d334db88b22192d43ffbff40c87890ab7ca5
sahilgoliwar25 <sahilgoliwar1@gmail.com>
2023-10-08 19:21:50 +0530

    new commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Poppins bg-weather-primary">
+    <SiteNavigation />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
+}
+</style>
