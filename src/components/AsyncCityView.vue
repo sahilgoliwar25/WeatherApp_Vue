dda77710550865c96cb47b4d8247aaddd28aa499
sahilgoliwar25 <sahilgoliwar1@gmail.com>
2023-10-09 12:43:46 +0530

    minor changes

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -125,7 +125,7 @@ const getWeatherData = async () => {
     const weatherData = await axios.get(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
     );
-    // console.log(weatherData);
+
     // cal current date & time
     const localOffset = new Date().getTimezoneOffset() * 60000;
     const utc = weatherData.data.current.dt * 1000 + localOffset;
